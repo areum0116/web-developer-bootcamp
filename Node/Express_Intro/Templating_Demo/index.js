a57e1e6d054b72442200206ea3c422392ff5bd72
areum0116 <areum0116@naver.com>
2023-06-06 19:01:24 +0900

    2023.6.6

--- Node/Express_Intro/Templating_Demo/index.js
+++ Node/Express_Intro/Templating_Demo/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -11,7 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
     const {subreddit} = req.params;
-    res.render('subreddit', {subreddit});
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit', {...data});
+    } else {
+        res.render('notfound', {subreddit});
+    }
 })
 
 app.get('/cats', (req, res) => {
