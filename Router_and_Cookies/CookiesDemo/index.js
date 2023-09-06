ac46cbe0b26a40b4d4fbbb1dec8efb2c455ba354
areum0116 <areum0116@naver.com>
2023-09-06 23:02:38 +0900

    2023.9.6

---
+++ Router_and_Cookies/CookiesDemo/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('thisismysecret'));
+
+app.get('/greet', (req, res) => {
+    const {name = 'No-name'} = req.cookies;
+    res.send(`Hey there, ${name}!`);
+})
+
+app.get('/setname', (req, res) => {
+    res.cookie('name', 'Areum');
+    res.cookie('animal', 'harlequin shrimp');
+    res.send("Sent you a cookie!");
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', {signed: true});
+    res.send('OK SIGNED YOUR FURIT COOKIE');
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.send(req.signedCookies);
+})
+
+app.listen(3000, ()=>{
+    console.log("SERVING!");
+})
